1240dc4cf13ce3f35862631c44d1a204fe0441cb
rodrigo <rodrigocsilva63@gmail.com>
2023-01-17 19:16:10 -0300

    add transaction object

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,50 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Nike air jordan',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Umbrella',
+      amount: 49.99,
+      date: DateTime.now(),
+    )
+  ];
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Expense planner"),
-      ),
-      body: Center(
-        child: Text('Widget'),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Expenses planner"),
+        ),
+        body: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              child: Card(
+                elevation: 5,
+                color: Colors.blue,
+                child: Text('Chart'),
+              ),
+            ),
+            Column(
+                children: transactions.map((tx) {
+              return Card(
+                child: Text(tx.title),
+              );
+            }).toList())
+          ],
+        ),
       ),
     );
   }
